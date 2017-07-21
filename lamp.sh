b018309197ffad740368ceb49ced8de31e8d7dd9
Kenta Saito <kenta.saito@sagittar.org>
2017-07-21 11:03:35 +0900

    2017-07-21 11:03:35

--- lamp.sh
+++ lamp.sh
@@ -57,14 +57,10 @@ touch /var/www/.htdigest
 chown www-data:www-data /var/www/.htdigest
 
 # MySQL5.7
-echo "mysql-server mysql-server/root_password password $MYSQLPW" | sudo debconf-set-selections
-echo "mysql-server mysql-server/root_password_again password $MYSQLPW" | sudo debconf-set-selections
 apt install -y mysql-server
 cp -a /etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf.orig
-sed -i 's/\[mysqld\]/[mysqld]\ncharacter-set-server = utf8/' /etc/mysql/mysql.conf.d/mysqld.cnf
-echo 'UPDATE `mysql`.`user` SET `plugin`="mysql_native_password" WHERE `User`="root"; FLUSH PRIVILEGES;' | mysql -p$MYSQLPW
+sed -i 's/\[mysqld\]/[mysqld]\nskip-grant-tables\ncharacter-set-server = utf8/' /etc/mysql/mysql.conf.d/mysqld.cnf
 service mysql restart
-echo 'SET password FOR root@localhost=password("");' | mysql -p$MYSQLPW
 
 # PHP7.0
 apt install -y libapache2-mod-php7.0
